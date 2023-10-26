87931c9e5ff350959266783e106483ca16e5c70b
Jordan31 <jordan@crowesnest.io>
2023-10-26 00:07:07 -0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace ConsoleApp2 {
+    internal class Program {
+
+        static void Main(String[] args) {
+            // intial array of ints to sort and render 
+            int[] arr = { 120, 40, 30, 60, 0, 70, 10, 20, 50 };
+            uint frameRate = 30;
+
+            RenderWindow window = new RenderWindow(new VideoMode(1980, 600), "bob");
+            Color bgColor = new Color(Color.White);
+
+            // set the frame rate of the rendered window
+            // requires unsigned int
+            window.SetFramerateLimit(frameRate);
+
+            // A copy of the original array because we don't want to change the orig.
+            int[] rev_array = arr;
+            Rectangles rectShape = new Rectangles();
+            int x_pos = 10;
+            bool isSorted = false;
+            int nextElement = 0;
+            /*
+                * Display the original unsorted array.
+                * First, we have to create an array of objects (drawables) then we can display
+                * array of objects.
+                */
+            RectangleShape[] origObjArray = new RectangleShape[arr.Length];
+            // Now populate the drawable obj array with objs 
+            for (int i = 0; i < arr.Length; i++) {
+                origObjArray[i] = rectShape.Foo(arr[i], x_pos);
+                x_pos = x_pos + 15;
+
+            }
+
+            
+
+            while (window.IsOpen) {
+                x_pos = 10;
+                int xpos = 110;
+                int ypos = 300;
+                //display the orig array
+                for (int i = 0; i < origObjArray.Length; i++) {
+                    origObjArray[i].Position = new Vector2f(xpos, ypos);
+                    //xpos = xpos + 15;
+                    window.Draw(origObjArray[i]);                 
+                }
+
+                /*
+                * Bubble Sort
+                * Sort the array using bubble sort method and display the output real-time.
+                */
+                if (!isSorted) {
+                    for (int i = 0; i <= rev_array.Length - 1; i++) {
+                        for (int j = 0; j < rev_array.Length - i - 1; j++) {
+                            if (rev_array[j] > rev_array[j + 1]) {
+                                nextElement = rev_array[j + 1];
+                                rev_array[j + 1] = rev_array[j];
+                                rev_array[j] = nextElement;
+                            }
+                            window.Clear();
+                            window.Draw(rectShape.Foo(rev_array[j], x_pos));
+                            window.Display();
+                            x_pos = x_pos + 15;
+                        }
+                    }
+                    isSorted = true;
+                    // for debug only
+                    for (int k = 0; k < rev_array.Length; k++) {
+                        Console.WriteLine(rev_array[k]);
+                    }
+                }
+
+                // Display the window contents until the window is closed
+                //window.Display();
+                //window.Clear();
+            }
+
+
+
+            /*// Sort the array
+            for(int k = 0; k < shapeArray.Length; k++) {
+                Array.Sort(rev_array);
+                Console.WriteLine(rev_array[k]);
+            }*/
+
+            // part of the original unsorted stuff
+            // creat array for type rectangles
+            // original unsorted array of rectangle objects
+            /* RectangleShape[] shapeArray = new RectangleShape[9];
+
+             // fill array with rectangle objects
+             int x_pos = 10;
+             for (int i = 0; i < arr.Length; i++) {
+                 Rectangles bob = new Rectangles(arr[i], x_pos);
+                 x_pos = x_pos + 15;
+                 shapeArray[i] = bob.Foo();
+             }
+
+
+             RectangleShape[] rev_shapeArray = new RectangleShape[9];
+
+             for (int i = 0; i < rev_array.Length; i++) {
+                 Rectangles bob = new Rectangles(rev_array[i], x_pos);
+                 x_pos = x_pos + 15;
+                 rev_shapeArray[i] = bob.Foo();
+
+             }
+
+             // game loop or app loop to keep window and main thread running
+             while (window.IsOpen) {
+                 for(int j = 0; j < shapeArray.Length; j++) {
+                     window.Draw(shapeArray[j]);
+                 }
+
+                 for(int l = 0; l < rev_shapeArray.Length; l++) {
+                     window.Draw(rev_shapeArray[l]);
+                 }
+                 window.Display();
+                 window.Clear();
+             }*/
+        }
+    }
+}
---
+++ ConsoleApp2/Rectangles.cs
@@ -0,0 +1,54 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ConsoleApp2 {
+    class Rectangles : SFML.Graphics.Drawable {
+
+        protected int y_pos = 200;
+        protected int y_length;
+        protected int x_thick;
+        protected int cord_x;
+        protected int prev_x_cord;
+        protected int y;
+        protected int x;
+
+        void Drawable.Draw(SFML.Graphics.RenderTarget target, SFML.Graphics.RenderStates states) 
+        { 
+        }
+
+        public Rectangles() {
+            this.x_thick = 10; //hardcoded thickness of red border
+        }
+
+        public Rectangles(int y, int x) {
+            this.x_thick = 10; // hardcoded
+            this.y_length = y;
+            this.prev_x_cord = x;
+        }
+
+        public SFML.Graphics.RectangleShape Foo(int y, int x) {
+            this.y_length = y;
+            this.prev_x_cord = x;
+            RectangleShape square = new RectangleShape(new Vector2f(this.x_thick, this.y_length));
+            square.FillColor = new Color(Color.Green);
+            square.OutlineColor = new Color(Color.Red);
+            square.OutlineThickness = 2;
+            this.cord_x = prev_x_cord + 15;
+            square.Rotation = 180;
+            square.Position = new Vector2f(this.cord_x, 200);
+            return square;
+        }
+      /*  public Rectangles(RenderTarget target, RenderStates states) {
+            RectangleShape foo = new RectangleShape();
+            target.Draw(foo, states);
+        }*/
+
+    }
+}
